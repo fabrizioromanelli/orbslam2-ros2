b4276de8edc44b06e1e5cdaf2a90b683139bcd9e
Fabrizio Romanelli <fabrizio.romanelli@gmail.com>
2021-04-01 16:39:46 +0200

    Publishing orbslam2 state at 10Hz successful on topic /orbslam2_state

--- src/orbslam2.cpp
+++ src/orbslam2.cpp
@@ -1,24 +1,25 @@
 #include <memory>
 #include <chrono>
-#include <sensor_msgs/msg/imu.hpp>
-#include <sensor_msgs/msg/image.hpp>
 #include <cv_bridge/cv_bridge.h>
 #include <opencv2/core/core.hpp>
 #include <ORB_SLAM2/System.h>
 #include "rmw/qos_profiles.h"
 #include "std_msgs/msg/string.hpp"
+#include "std_msgs/msg/int32.hpp"
+#include <message_filters/subscriber.h>
+#include <message_filters/time_synchronizer.h>
+#include <message_filters/sync_policies/approximate_time.h>
+#include "rclcpp/rclcpp.hpp"
 
 rmw_qos_profile_t qos_profile = rmw_qos_profile_sensor_data;
-
-#include "rclcpp/rclcpp.hpp"
 
 using std::placeholders::_1;
 using namespace std::chrono_literals;
 
-class ORBSLAM2Subscriber : public rclcpp::Node
+class ORBSLAM2Node : public rclcpp::Node
 {
   public:
-    ORBSLAM2Subscriber(ORB_SLAM2::System* pSLAM, ORB_SLAM2::System::eSensor _sensorType)
+    ORBSLAM2Node(ORB_SLAM2::System* pSLAM, ORB_SLAM2::System::eSensor _sensorType)
     : Node("orbslam2_to_realsense_subscriber"), mpSLAM(pSLAM), sensorType(_sensorType)
     {
       auto qos = rclcpp::QoS(
@@ -27,37 +28,66 @@ class ORBSLAM2Subscriber : public rclcpp::Node
         qos_profile.depth
       ),
       qos_profile);
-      publisher_ = this->create_publisher<std_msgs::msg::String>("orbslam2_pose", qos);
-          timer_ = this->create_wall_timer(500ms, std::bind(&ORBSLAM2Subscriber::timer_callback, this));
+
+      // Create publishers with 100ms frequency
+      pose_publisher_  = this->create_publisher<std_msgs::msg::String>("orbslam2_pose", qos);
+          pose_timer_  = this->create_wall_timer(100ms, std::bind(&ORBSLAM2Node::timer_pose_callback, this));
+      state_publisher_ = this->create_publisher<std_msgs::msg::Int32>("orbslam2_state", qos);
+          state_timer_ = this->create_wall_timer(100ms, std::bind(&ORBSLAM2Node::timer_state_callback, this));
     }
+
+    void setPose(cv::Mat);
+    void setState(signed int);
 
   private:
+    void timer_pose_callback();
+    void timer_state_callback();
+
     ORB_SLAM2::System* mpSLAM;
     ORB_SLAM2::System::eSensor sensorType;
-    rclcpp::TimerBase::SharedPtr timer_;
-    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+    rclcpp::TimerBase::SharedPtr pose_timer_, state_timer_;
+    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr pose_publisher_;
+    rclcpp::Publisher<std_msgs::msg::Int32>::SharedPtr state_publisher_;
     cv::Mat orbslam2Pose;
-
-    // Methods
-    void timer_callback()
-    {
-      auto message = std_msgs::msg::String();
-      std::cout << orbslam2Pose << std::endl << std::flush;
-      message.data = "Publishing "; // + std::to_string("a");
-      RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
-      publisher_->publish(message);
-    }
+    signed int orbslam2State = ORB_SLAM2::Tracking::eTrackingState::SYSTEM_NOT_READY;
 };
+
+void ORBSLAM2Node::setPose(cv::Mat _pose)
+{
+  orbslam2Pose = _pose;
+}
+
+void ORBSLAM2Node::setState(signed int _state)
+{
+  orbslam2State = _state;
+}
+
+void ORBSLAM2Node::timer_pose_callback()
+{
+  auto message = std_msgs::msg::String();
+  std::cout << orbslam2Pose << std::endl << std::flush;
+  message.data = "Publishing "; // + std::to_string("a");
+  RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
+  pose_publisher_->publish(message);
+}
+
+void ORBSLAM2Node::timer_state_callback()
+{
+  auto message = std_msgs::msg::Int32();
+  message.data = orbslam2State;
+  state_publisher_->publish(message);
+}
 
 class ImageGrabber
 {
   public:
-    ImageGrabber(ORB_SLAM2::System* pSLAM):mpSLAM(pSLAM){}
+    ImageGrabber(ORB_SLAM2::System* pSLAM, std::shared_ptr<ORBSLAM2Node> pORBSLAM2Node) : mpSLAM(pSLAM), mpORBSLAM2Node(pORBSLAM2Node){}
 
     void GrabRGBD(const sensor_msgs::msg::Image::SharedPtr& msgRGB, const sensor_msgs::msg::Image::SharedPtr& msgD);
     void GrabStereo(const sensor_msgs::msg::Image::SharedPtr& msgLeft, const sensor_msgs::msg::Image::SharedPtr& msgRight);
 
     ORB_SLAM2::System* mpSLAM;
+    std::shared_ptr<ORBSLAM2Node> mpORBSLAM2Node;
 };
 
 void ImageGrabber::GrabRGBD(const sensor_msgs::msg::Image::SharedPtr& msgRGB, const sensor_msgs::msg::Image::SharedPtr& msgD)
@@ -84,7 +114,8 @@ void ImageGrabber::GrabRGBD(const sensor_msgs::msg::Image::SharedPtr& msgRGB, co
   }
 
   rclcpp::Time Ts = cv_ptrRGB->header.stamp;
-  orbslam2Pose = mpSLAM->TrackRGBD(cv_ptrRGB->image, cv_ptrD->image, Ts.seconds());
+  mpORBSLAM2Node->setPose(mpSLAM->TrackRGBD(cv_ptrRGB->image, cv_ptrD->image, Ts.seconds()));
+  mpORBSLAM2Node->setState(mpSLAM->GetTrackingState());
 }
 
 void ImageGrabber::GrabStereo(const sensor_msgs::msg::Image::SharedPtr& msgLeft, const sensor_msgs::msg::Image::SharedPtr& msgRight)
@@ -111,7 +142,8 @@ void ImageGrabber::GrabStereo(const sensor_msgs::msg::Image::SharedPtr& msgLeft,
   }
 
   rclcpp::Time Ts = cv_ptrLeft->header.stamp;
-  orbslam2Pose = mpSLAM->TrackStereo(cv_ptrLeft->image, cv_ptrRight->image, Ts.seconds());
+  mpORBSLAM2Node->setPose(mpSLAM->TrackStereo(cv_ptrLeft->image, cv_ptrRight->image, Ts.seconds()));
+  mpORBSLAM2Node->setState(mpSLAM->GetTrackingState());
 }
 
 enum string_code {
@@ -150,9 +182,9 @@ int main(int argc, char * argv[])
   }
 
   ORB_SLAM2::System SLAM(argv[1], argv[2], sensorType, true);
-  auto nodePtr = std::make_shared<ORBSLAM2Subscriber>(&SLAM, sensorType);
+  auto nodePtr = std::make_shared<ORBSLAM2Node>(&SLAM, sensorType);
 
-  ImageGrabber igb(&SLAM);
+  ImageGrabber igb(&SLAM, nodePtr);
   std::string s1, s2;
 
   if (sensorType == ORB_SLAM2::System::RGBD)
